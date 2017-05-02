af91b57db9d117b66204ba0fd28edb2ac9ea1080
ayushkhanvilkar10@gmail.com <ayushkhanvilkar96@gmail.com>
2017-05-02 14:23:40 +0530

    .

---
+++ BankIntf.java
@@ -0,0 +1,7 @@
+import java.rmi.*; 
+public interface BankIntf extends Remote 
+{ 
+int withdraw(int a,int amt)throws RemoteException; 
+int deposit(int b,int amt)throws RemoteException; 
+int balance(int amt)throws RemoteException; 
+} 
